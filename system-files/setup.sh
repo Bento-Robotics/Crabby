2d64ca3725873344970c61e5cd6123b1806413e2
snaens <snaens@snaens.net>
2025-07-30 02:12:29 +0200

    rework system setup script
    reworked setup.sh to utilize bash functions better and use the
    `install` command

--- system-files/setup.sh
+++ system-files/setup.sh
@@ -2,55 +2,23 @@ GREEN='\033[1;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
 
-set_owner_and_group_to_root () {
-  sudo chown root "$1"
-  sudo chgrp root "$1"
+# (permissions, user, group, destination)
+# source is assumed to be "./destination"
+install_thingy() {
+  if [ -e $4 ]; then
+    printf "${YELLOW}WARN: file '$4' already exists. Overwrite? (y/n)${NC}%s "
+    read -r YN
+  else
+    YN="Y"
+  fi
+  if [ "$YN" = "y" ]||[ "$YN" = "Y" ]; then
+    install -D -m $1 -o $2 -g $3 "./$4" $4
+    printf "${GREEN}OK: $4${NC}%s "
+  fi
 }
 
-DEST_SYSCON="/etc/NetworkManager/system-connections/Bento-Box_WiFi.nmconnection"
-if [ -e $DEST_SYSCON ]; then
-  printf "${YELLOW}WARN: file '$DEST_SYSCON' already exists. Overwrite? (y/n)${NC}%s "
-  read -r YN
-else
-  YN="Y"
-fi
-if [ "$YN" = "y" ]||[ "$YN" = "Y" ]; then
-  cp "./etc/NetworkManager/system-connections/Bento-Box_WiFi.nmconnection" $DEST_SYSCON
-  chmod 600 $DEST_SYSCON
-  set_owner_and_group_to_root $DEST_SYSCON
-  printf "${GREEN}OK: $DEST_SYSCON${NC}%s "
-fi
-
-
-DEST_DNSMASQ_SHARED="/etc/NetworkManager/dnsmasq-shared.d/bento-box.conf"
-if [ -e $DEST_DNSMASQ_SHARED ]; then
-  printf "${YELLOW}WARN: file '$DEST_DNSMASQ_SHARED' already exists. Overwrite? (y/n)${NC}%s "
-  read -r YN
-else
-  YN="Y"
-fi
-if [ "$YN" = "y" ]||[ "$YN" = "Y" ]; then
-  cp "./etc/NetworkManager/dnsmasq-shared.d/bento-box.conf" $DEST_DNSMASQ_SHARED
-  chmod 644 $DEST_DNSMASQ_SHARED
-  set_owner_and_group_to_root $DEST_DNSMASQ_SHARED
-  printf "${GREEN}OK: $DEST_DNSMASQ_SHARED${NC}%s "
-fi
-
-
-DEST_SYSD_CAN="/etc/systemd/network/80-can.network"
-if [ -e $DEST_SYSD_CAN ]; then
-  printf "${YELLOW}WARN: file '$DEST_SYSD_CAN' already exists. Overwrite? (y/n)${NC}%s "
-  read -r YN
-else
-  YN="Y"
-fi
-if [ "$YN" = "y" ]||[ "$YN" = "Y" ]; then
-  cp "./etc/systemd/network/80-can.network" $DEST_SYSD_CAN
-  chmod 644 $DEST_SYSD_CAN
-  set_owner_and_group_to_root $DEST_SYSD_CAN
-  printf "${GREEN}OK: $DEST_SYSD_CAN${NC}%s "
-fi
-
+install_thingy 600 root root "/etc/NetworkManager/system-connections/Crabby_WiFi.nmconnection"
+install_thingy 644 root root "/etc/systemd/network/80-can.network"
 
 printf "${GREEN}set wifi region? (y/n)${NC}%s "
 read -r YN
